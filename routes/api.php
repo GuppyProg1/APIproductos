bf32be04559db33690915493e5842bc00d4f1a10
Guppy <alejo.diaz.ruiz@gmail .com>
2026-03-09 09:34:50 -0500

    se agregaron las rutas get, put, y delete en api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ Route::post('login',[AuthController::class, 'login']);
 //Autenticadas
 Route::middleware('auth:api')->group(function () {
 
+    // GET /productos/{id} -> show (acceso: admin y usuario)
+    Route::get('productos/{id}', [ProductoBaseController::class, 'show'])->middleware('role:admin,usuario');
+
+    // PUT /productos/{id} -> update (acceso: solo admin)
+    Route::put('productos/{id}', [ProductoBaseController::class, 'update'])->middleware('role:admin');
+
+    // DELETE /productos/{id} -> destroy (acceso: solo admin)
+    Route::delete('productos/{id}', [ProductoBaseController::class, 'destroy'])->middleware('role:admin');
+
+
     Route::get('me', [AuthController::class, 'me']);
 
     //solo consulta: admin y usuario
