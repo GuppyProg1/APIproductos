1296ba3fc6f253e01c624a992af5b6b80b918ac8
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-03-13 09:47:28 -0500

    creamos y agregamos codigo html en el archivo producto_creado

---
+++ resources/views/email/producto_creado.blade.php
@@ -0,0 +1,12 @@
+
+<h2>Nuevo producto creado</h2>
+
+<p>Se ha registrado un nuevo producto en el sistema.</p>
+
+<ul>
+    <li><strong>Nombre:</strong> {{ $producto->nombre }}</li>
+    <li><strong>Precio:</strong> {{ $producto->precio }}</li>
+    <li><strong>Stock:</strong> {{ $producto->stock }}</li>
+</ul>
+
+<p>ApiProductos</p>
