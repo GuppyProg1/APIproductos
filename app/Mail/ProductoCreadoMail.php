2a8c311966cb9d9773194f5ada932b504886c67f
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-03-13 09:44:46 -0500

    Creamos la clase producto creado para que se envia via email

---
+++ app/Mail/ProductoCreadoMail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Bus\Queueable;
+
+class ProductoCreadoMail extends Mailable{
+    use Queueable, SerializesModels;
+
+    public $producto;
+
+    public function __construct($producto){
+        $this->producto = $producto;
+    }
+
+    public function build(){
+        return $this->subject('Nuevo producto creado')
+                    ->view('emails.producto_creado');
+    }
+}
